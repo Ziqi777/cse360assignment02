612a259245a6fdba8f47bb9c4d287d8f5f9e8464
ziqigong <ziqigong@192.168.1.14>
2020-10-05 23:17:10 -0700

    Version 2.0; updated one with implemented codes.

--- CSE360ASSIGNMENT1/src/cse360assignment02/AddingMachine.java
+++ CSE360ASSIGNMENT1/src/cse360assignment02/AddingMachine.java
@@ -1,31 +1,38 @@
 /**
- * @ CSE360 Assignment 1, unimplemented
+ * @ CSE360 Assignment 1, implemented file
  * @author Ziqi Gong
- * @version 1.0
+ * @version 2.0
  */
 package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  
+  private String totalString = "0";
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  totalString+=" + "+value;
+	  total+=value;
   }
 
   public void subtract (int value) {
+	  totalString+=" - "+value;
+	  total-=value;
+	  
   }
 
   public String toString () {
-    return "";
+    return totalString;
   }
 
   public void clear() {
+	  total = 0;
+	  totalString = "0";
   }
 }
